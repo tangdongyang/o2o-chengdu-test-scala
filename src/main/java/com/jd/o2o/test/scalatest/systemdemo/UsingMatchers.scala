b7fa87b77938a5a40868e8f12290ce21eb25ece9
cdtdy <tangdongyang@jd.com>
2015-09-17 10:46:43 +0800

    update use matcher

--- src/main/java/com/jd/o2o/test/scalatest/systemdemo/UsingMatchers.scala
+++ src/main/java/com/jd/o2o/test/scalatest/systemdemo/UsingMatchers.scala
@@ -3,16 +3,20 @@ package com.jd.o2o.test.scalatest.systemdemo
 import java.io.File
 import java.util
 
-import org.scalatest.matchers.{MatchResult, BeMatcher, BePropertyMatchResult, BePropertyMatcher}
+import org.scalatest.matchers._
 import org.scalatest.{Matchers, FlatSpec}
 import org.scalactic.StringNormalizations._
 import org.scalatest._
 import Inspectors._
+import CustomEndWithExtensionMatchers._
+import MatcherProducers._
 
 /**
  * Created by Administrator on 2015/9/7.
  */
-class UsingMatchers extends FlatSpec with Matchers with CustomBePropertyMatcher with CustomBeMatcher {
+class UsingMatchers extends FlatSpec
+                    with Matchers with CustomBePropertyMatcher with CustomBeMatcher
+                    with CustomHavePropertyMatcher with Inside {
 
   // Checking equality with matchers
   val result = 3
@@ -206,7 +210,233 @@ class UsingMatchers extends FlatSpec with Matchers with CustomBePropertyMatcher 
 
   // be as an equality comparison
   // result should equal (null)  // the same as: result should be (null)
+  7.0 should be (7.0)
+  false should be (false)
+  true should be (true)
+  Nil should be (Nil)
+  None should be (None)
+  Some(1) should be (Some(1))
 
+  Array(1, 2) should be (Array(1, 2))
+
+  // Being negative
+  List(1) should not be (null)
+  14 should not be <= (10)
+  List(1) should not be (List(2))
+  "HaHa" should not startWith ("Hello")
+
+  // Checking that a snippet of code does not compile
+  "val a: String = 1" shouldNot compile
+  "val a: String = l" shouldNot typeCheck
+  "val a: Int = 1" should compile
+
+  // Logical expressions with and and or
+  // The other difference with Boolean operators is that although && has a higher precedence than ||,
+  // and and or have the same precedence.
+  // Thus although the Boolean expression (a || b && c) will evaluate the && expression before the || expression, like (a || (b && c))
+  Map("one" -> 1, "two" -> 2) should (contain key ("two") and not contain value (7))
+  4 should (be > (0) and be <= (10))
+  None should (equal (Some(List(1, 2, 3))) or be (None))
+  "fum" should (
+    equal ("fee") or
+    equal ("fie") or
+    equal ("fum")
+    )
+  "yellow" should (equal ("yellow") or equal {println("Hello, World!"); "yellow"})
+
+  // Working with Options
+  None shouldEqual None
+  None shouldBe None
+  None should === (None)
+  None shouldBe empty
+
+  Some("hi") shouldEqual Some("hi")
+  Some("hi") shouldBe Some("hi")
+  Some("hi") should === (Some("hi"))
+
+  Some("hi") shouldBe defined
+
+  Some(2) should contain (2)
+  Some(2) should contain oneOf (2, 3, 4)
+  Some(2) should contain noneOf (5, 6, 7)
+
+  // Checking arbitrary properties with have
+  tdy002 should have (
+    'tester (false),
+    'age (25)
+  )
+
+  val book = new Book("ScalaTest", "tdy002", 220)
+  book should have (
+    title ("ScalaTest"),
+    author ("tdy002")
+  )
+
+  // Using length and size with HavePropertyMatchers
+  book should have (
+    title ("ScalaTest"),
+    author ("tdy002"),
+    length (220)
+  )
+
+  // Checking that an expression matches a pattern
+  val name = new Name("Jane", "Q", "Scala")
+  inside(name) { case Name(first, _, _) =>
+    first should startWith ("J")
+  }
+  name should matchPattern { case Name("Jane", _, _) => }
+
+  val record = Record(
+    Name("tdy001", "tdy002", "tdy003"),
+    Address("street", "city", "state", "zip"),
+    25
+  )
+  inside(record) { case Record(name, address, age) =>
+    inside(name) { case Name(first, middle, last) =>
+      first should be ("tdy001")
+      middle should be ("tdy002")
+      last should be ("tdy003")
+    }
+
+    inside(address) { case Address(street, city, state, zip) =>
+      street should startWith ("str")
+      city should endWith ("ty")
+      state should equal ("state")
+      zip should be ("zip")
+    }
+
+    age should be < 99
+  }
+
+  // Using custom matchers
+  val file_txt = File.createTempFile("hello", "txt")
+  file_txt.getName should endWith ("txt")
+
+  file_txt should endWithExtension ("txt")
+  // file_txt should not endWithExtension "ppt"
+  // file_txt should (exist and endWithExtension ("txt"))
+
+  // Creating dynamic matchers
+
+  // Creating matchers using logical operators
+  val beWithinTolerance = be >= 0 and be <= 10
+  7 should beWithinTolerance
+
+  val beOdd_1 = new Matcher[Int] {
+
+    def apply(left: Int) = MatchResult(
+      left % 2 == 1,
+      left + " was not odd",
+      left + " was odd"
+    )
+  }
+
+  val beOdd = Matcher { (left: Int) =>
+    MatchResult(
+      left % 2 == 1,
+      left + " was not odd",
+      left + " was odd"
+    )
+  }
+
+  3 should beOdd
+  4 should not (beOdd)
+
+  // Composing matchers
+  def endWithExtension(ext: String) = endWith(ext) compose { (file: File) => file.getPath}
+  new File("output.txt") should endWithExtension("txt")
+
+  val f = be > (_: Int)
+  val g = (_: String).toInt
+
+  val beAsIntsGreaterThan = (f compose g) andThen (_ compose g)
+  "8" should beAsIntsGreaterThan ("7")
+
+  val beAsIntsGreaterThan2 = f composeTwice g // means: (f compose g) andThen (_ compose g)
+  "8" should beAsIntsGreaterThan2 ("7")
+
+  val beAsIntsGreaterThan3 = f composeTwice g mapResult { mr =>
+    mr.copy(
+      failureMessageArgs = mr.failureMessageArgs.map(LazyArg(_) {"\"" + _.toString + "\".toInt"}),
+      negatedFailureMessageArgs = mr.negatedFailureMessageArgs.map(LazyArg(_) {"\"" + _.toString + "\".toInt"}),
+      midSentenceFailureMessageArgs = mr.midSentenceFailureMessage.map(LazyArg(_) {"\"" + _.toString + "\".toInt"}),
+      midSentenceNegatedFailureMessageArgs = mr.midSentenceNegatedFailureMessage.map(LazyArg(_) {"\"" + _.toString + "\".toInt"})
+    )}
+
+  "8" should beAsIntsGreaterThan3 ("7")
+
+  // Checking for expected exceptions
+  an [IndexOutOfBoundsException] should be thrownBy "HelloWorld".charAt(-1)
+
+  val thrown = the [IndexOutOfBoundsException] thrownBy "HelloWorld".charAt(-1)
+  thrown.getMessage should equal ("String index out of range: -1")
+
+  the [ArithmeticException] thrownBy 1 / 0 should have message "/ by zero"
+  the [IndexOutOfBoundsException] thrownBy {
+    "HelloWorld".charAt(-1)
+  } should have message "String index out of range: -1"
+
+  noException should be thrownBy 0 / 1
+
+  // Those pesky parens
+  // Although you don't always need them, you can choose to always put parentheses around right-hand values, such as the 7 in num should equal (7):
+  // Except for length and size, you must always put parentheses around the list of one or more property values following a have
+  // You must always put parentheses around and and or expressions, as in
+}
+
+trait CustomEndWithExtensionMatchers {
+
+  class FileEndsWithExtensionMatcher(expectedExtension: String) extends Matcher[File] {
+
+    def apply(left: File) = {
+      val name = left.getName
+      MatchResult(
+        name.endsWith(expectedExtension),
+        s"""File $name did not end with extension "$expectedExtension"""",
+        s"""File $name ended with extension "$expectedExtension""""
+      )
+    }
+  }
+
+  def endWithExtension(expectedExtension: String) = new FileEndsWithExtensionMatcher(expectedExtension)
+}
+
+object CustomEndWithExtensionMatchers extends CustomEndWithExtensionMatchers
+
+case class Name(val first: String, val middle: String, val last: String)
+case class Address(val street: String, val city: String, val state: String, val zip: String)
+case class Record(val name: Name, val address: Address, val age: Int)
+
+case class Book(val title: String, val author: String, val length: Int)
+
+trait CustomHavePropertyMatcher {
+
+  def title(expectedValue: String) = new HavePropertyMatcher[Book, String] {
+    def apply(book: Book) = HavePropertyMatchResult(
+      book.title == expectedValue,
+      "title",
+      expectedValue,
+      book.title
+    )
+  }
+
+  def author(expectedValue: String) = new HavePropertyMatcher[Book, String] {
+    def apply(book: Book) = HavePropertyMatchResult(
+      book.author == expectedValue,
+      "author",
+      expectedValue,
+      book.author
+    )
+  }
+
+  def length(expectedValue: Int) = new HavePropertyMatcher[Book, Int] {
+    def apply(book: Book) = HavePropertyMatchResult(
+      book.length == expectedValue,
+      "length",
+      expectedValue,
+      book.length
+    )
+  }
 }
 
 trait CustomBeMatcher {
@@ -250,6 +480,7 @@ trait CustomBePropertyMatcher {
 
 class Tester {
   var tester: Boolean = false
+  val age: Int = 25
 
   def isTester(): Boolean = {
     tester
